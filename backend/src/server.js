942a66eb2d83f0618b17f94bb46e1ee27369b8cd
Marce <228049936+MarceEsquinas@users.noreply.github.com.>
2026-06-02 12:18:14 +0200

    crear servidor

---
+++ backend/src/server.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('la cosa va bien');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
